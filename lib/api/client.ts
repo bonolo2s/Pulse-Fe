6ebc52ecea9f1ba3aa15d76e4d059bb6af00e434
Bonolo Selowa <bonololloyd003@gmail.com>
2026-07-01 16:35:46 +0200

    Api Client

---
+++ lib/api/client.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  timeout: 30000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+
+    if (error.code === "ECONNABORTED") {
+      console.error("Request timeout");
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
