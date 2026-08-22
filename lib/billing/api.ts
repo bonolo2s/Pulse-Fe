a176f7112e3027325c247b09f2c00edd21749833
Bonolo Selowa <bonololloyd003@gmail.com>
2026-08-22 23:38:13 +0200

    paymentMethod Apis

--- lib/billing/api.ts
+++ lib/billing/api.ts
@@ -1,6 +1,6 @@
 import apiClient from "../shared/client"
 import { isAxiosError } from "axios"
-import { InitializeTransactionResult, Subscription, Invoice } from "./types"
+import { InitializeTransactionResult, Subscription, Invoice, PaymentMethod } from "./types"
 import { ApiResponse } from "../shared/types"
 
 export const initiateCheckout = async (): Promise<ApiResponse<InitializeTransactionResult>> => {
@@ -42,6 +42,42 @@ export const getBillingHistory = async (userId: string): Promise<ApiResponse<Inv
 export const cancelSubscription = async (userId: string): Promise<ApiResponse<void>> => {
   try {
     const response = await apiClient.put<ApiResponse<void>>(`/billing/cancel-subscription/${userId}`)
+    return response.data
+  } catch (err) {
+    if (isAxiosError(err) && err.response?.data) {
+      return err.response.data as ApiResponse<void>
+    }
+    throw err
+  }
+}
+
+export const getPaymentMethods = async (userId: string): Promise<ApiResponse<PaymentMethod[]>> => {
+  try {
+    const response = await apiClient.get<ApiResponse<PaymentMethod[]>>(`/billing/get-payment-methods/${userId}`)
+    return response.data
+  } catch (err) {
+    if (isAxiosError(err) && err.response?.data) {
+      return err.response.data as ApiResponse<PaymentMethod[]>
+    }
+    throw err
+  }
+}
+
+export const deletePaymentMethod = async (id: string): Promise<ApiResponse<void>> => {
+  try {
+    const response = await apiClient.delete<ApiResponse<void>>(`/billing/payment-methods/${id}`)
+    return response.data
+  } catch (err) {
+    if (isAxiosError(err) && err.response?.data) {
+      return err.response.data as ApiResponse<void>
+    }
+    throw err
+  }
+}
+
+export const setDefaultPaymentMethod = async (id: string): Promise<ApiResponse<void>> => {
+  try {
+    const response = await apiClient.put<ApiResponse<void>>(`/billing/payment-methods/${id}/set-default`)
     return response.data
   } catch (err) {
     if (isAxiosError(err) && err.response?.data) {
