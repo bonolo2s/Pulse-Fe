44922a52084dd3b0a56df60d755bbb469a9899c3
Bonolo Selowa <bonololloyd003@gmail.com>
2026-09-14 13:47:10 +0200

    fix login page hydration mismatch caused by browser extension

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${jetbrainsMono.variable} font-sans antialiased`}>
+      <body suppressHydrationWarning className={`${inter.variable} ${jetbrainsMono.variable} font-sans antialiased`}>
         {children}
         <Analytics />
       </body>
