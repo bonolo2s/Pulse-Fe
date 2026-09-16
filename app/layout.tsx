e9817b058dea7bfa4835185beaf003a7dba6eaf8
Bonolo Selowa <bonololloyd003@gmail.com>
2026-09-16 15:36:07 +0200

    favicon replcament

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,19 +12,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/pulse.jpg',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/pulse.jpg',
         media: '(prefers-color-scheme: dark)',
       },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
     ],
-    apple: '/apple-icon.png',
+    apple: '/pulse.jpg',
   },
 }
 
